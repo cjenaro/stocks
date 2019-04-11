9d3e3a05def079842c6aaa749d6bdc2483554d6f
Jenaro Calviño <jen.calvineo@live.com.ar>
2019-04-10 22:33:08 -0300

    Initial dist subtree

---
+++ js/about.2bf51376.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{b236:function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div")},l=[],a={},c=a,o=t("2877"),r=Object(o["a"])(c,u,l,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=about.2bf51376.js.map
